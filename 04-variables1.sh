7a2ee1b6c65a088ba8cd5758170b7606913d5b4a
suri2285 <yvsureshsv93@gmail.com>
2024-09-03 12:41:14 +0530

    variables

---
+++ 04-variables1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+X = Ramesh
+Y - suresh
+
+echo "$X:: Hi $Y,How are you?"
+echo "$Y::Hello $X.I am fine.How are you doing?"
+echo  "$X::I am doing good $Y.what's going on?"
+echo  "$Y::I started learning shell script $X"
